a8779e6e1489bc1018102960bc6bfdac014670b2
neiwad <hello@neiwad.dev>
2023-01-02 14:56:52 +0100

    remove /dist folder from repository

--- dist/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { Pwdckr } from "./pwdckr";
--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Pwdckr = void 0;
-var pwdckr_1 = require("./pwdckr");
-Object.defineProperty(exports, "Pwdckr", { enumerable: true, get: function () { return pwdckr_1.Pwdckr; } });
